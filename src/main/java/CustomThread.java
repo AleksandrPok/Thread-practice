537b4bddd1719eb9cdc66e38c4a06a0f16476fce
OleksandrPokotylenko <aln.nko.of@gmail.com>
2021-02-23 17:12:52 +0200

    removed magic numbers; added local variable in method run() in CustomRunnable & CustomThread classes;

--- src/main/java/CustomThread.java
+++ src/main/java/CustomThread.java
@@ -2,6 +2,7 @@ import org.apache.log4j.Logger;
 
 public class CustomThread extends Thread {
     private static final Logger LOGGER = Logger.getLogger(CustomThread.class);
+    private static final int MAX_COUNT = 100;
     private Counter counter;
 
     public CustomThread(Counter counter) {
@@ -10,8 +11,9 @@ public class CustomThread extends Thread {
 
     @Override
     public void run() {
-        while (counter.getCount() < 100) {
-            counter.setCount(counter.getCount() + 1);
+        while (counter.getCount() < MAX_COUNT) {
+            int value = counter.getCount() + 1;
+            counter.setCount(value);
             LOGGER.info(Thread.currentThread().getName() + " = " + counter.getCount());
         }
     }
