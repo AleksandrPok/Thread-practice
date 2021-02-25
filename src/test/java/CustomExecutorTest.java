e6585cad6a5909f1456b934c1a44cd40a3a91d49
OleksandrPokotylenko <aln.nko.of@gmail.com>
2021-02-25 12:23:04 +0200

    removed unnecessary dependency; added empty lines in CustomExecutorTest & CustomForkJoinTest; removed expected initialisation to setUp()

--- src/test/java/CustomExecutorTest.java
+++ src/test/java/CustomExecutorTest.java
@@ -7,17 +7,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class CustomExecutorTest {
     private static List<Integer> list;
     private static CustomExecutor customExecutor;
+    private static Integer expected;
 
     @BeforeAll
     public static void setUp() {
         list = new NumbersProducer().getIntegerList();
         customExecutor = new CustomExecutor(list);
+        expected = NumbersProducer.getIntegerList().stream()
+                .reduce(Integer::sum).get();
     }
 
     @Test
     public void getSum_Ok() {
-        Integer expected = NumbersProducer.getIntegerList().stream()
-                .reduce(Integer::sum).get();
         Integer actual = customExecutor.getSum();
         assertEquals(expected, actual, "Expected: " + expected + "Actual: " + actual);
     }
--- src/test/java/CustomForkJoinTest.java
+++ src/test/java/CustomForkJoinTest.java
@@ -7,17 +7,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class CustomForkJoinTest {
     private static List<Integer> list;
     private static CustomForkJoin customForkJoin;
+    private static Integer expected;
 
     @BeforeAll
     public static void setUp() {
         list = new NumbersProducer().getIntegerList();
         customForkJoin = new CustomForkJoin(list);
+        expected = NumbersProducer.getIntegerList().stream()
+                .reduce(Integer::sum).get();
     }
 
     @Test
     public void compute_Ok() {
-        Integer expected = NumbersProducer.getIntegerList().stream()
-                .reduce(Integer::sum).get();
         Integer actual = customForkJoin.compute();
         assertEquals(expected, actual, "Expected: " + expected + "Actual: " + actual);
     }
